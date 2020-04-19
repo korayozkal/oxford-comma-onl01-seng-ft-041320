906b4e2fb87aeb00aa6d3b4505ef6004eb1d7fe5
Koray Ozkal <koryozkal@gmail.com>
2020-04-19 00:59:39 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,11 @@ elsif array.length == 2
 [array].join(" and ")
 
 elsif array.length == 3
-array_new= array.join(", ") array[2].joi, n("and ")[3].join
+array.join(", ") << and
+array.insert 
+
+
+array[2].joi, n("and ")[3].join
 
 end
 end
